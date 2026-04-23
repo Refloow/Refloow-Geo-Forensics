3003c07dbf725f5225fd8e6a632d3ca19d35ee29
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2026-04-23 22:47:09 +0200

    Adding a preload function for getting the app version #26

--- src/preload.js
+++ src/preload.js
@@ -2,5 +2,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    triggerUpdateCheck: (isAutoUpdateEnabled) => ipcRenderer.send('check-updates', isAutoUpdateEnabled)
+    triggerUpdateCheck: (isAutoUpdateEnabled) => ipcRenderer.send('check-updates', isAutoUpdateEnabled),
+    getAppVersion: () => ipcRenderer.invoke('get-app-version')
 });
